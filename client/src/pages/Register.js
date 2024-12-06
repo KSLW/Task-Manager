a1d3b695bbf2767df648464b75e8930b4892b674
Kirk SL Wilkinson <KSLW1997@outlook.com>
2024-12-06 11:31:15 +0000

    Update backend paths

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -9,7 +9,7 @@ const Register = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            await axios.post(`${process.env.REACT_APP_API_URL}/api/auth/register`, { name, email, password });
+            await axios.post(`${process.env.REACT_APP_API_URL}api/auth/register`, { name, email, password });
             alert('Registration successful!');
             window.location.href = '/';
         } catch (err) {
